0d44cdab4bc9fdb1e7faed5268075971d28ab02d
davidmasp <david.mas@irbbarcelona.org>
2017-08-28 18:15:04 +0200

    R update

--- R/funs.R
+++ R/funs.R
@@ -1,13 +1,26 @@
+# optimals
+# pb = create_stairs
+# pipeing %>% pb
+# plot(pb)
 
-create_stairs <- function(name=NULL){
-  stairs_object <<- structure(data.frame(),class="stairs")
-}
-
-add_stair <- function(df,stairs,name = "Sample"){
-  n = nrow(df)
-  tmp = data.frame(step = name,n = n)
-  stairs_object <<- rbind(stairs,tmp)
+create_stairs <- function(df){
+  attributes(df)$stairs <- data.frame(step = c(),n = c())
   return(df)
 }
 
 
+set_stairs <- function(df,name = "sample"){
+  ntmp = nrow(df)
+  tmp = data.frame(step = name,n = ntmp)
+  attributes(df)$stairs <- rbind(attributes(df)$stairs,tmp)
+  return(df)
+}
+
+get_stairs <- function(df) {
+  if(!"stairs" %in% names(attributes(df))){
+    stop("DF has not been initiated, use create_stairs(df)")
+  }
+  st <- attributes(df)$stairs
+  class(st) <- "stairs"
+  return(st)
+}
--- R/plotting.R
+++ R/plotting.R
@@ -1,9 +1,11 @@
-plot.stairs <- function(df){
+
+plot_stairs <- function(df){
   require(ggplot2)
   require(forcats)
-  x$step <- fct_reorder(f = step,x = n,fun = max)
-  g = ggplot(data = df, aes(x = step,
-                        y = n)) +
+  st = attributes(df)$stairs
+  st$step <- fct_reorder(f = st$step,x = st$n,fun = max,.desc = T)
+  g = ggplot(data = st, aes(x = step,
+                            y = n)) +
     geom_bar(stat = "identity")
   return(g)
 }
